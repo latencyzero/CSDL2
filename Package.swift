d5590248dab4af8c31eeb08470262f065112037d
Rick Mann <rmann@latencyzero.com>
2019-08-25 02:33:44 -0700

    First checkin.

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+	name: "CSDL2",
+	pkgConfig: "sdl2",
+	providers: [
+		.brew(["sdl2"])
+	]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# CSDL2
+
+A description of this package.
